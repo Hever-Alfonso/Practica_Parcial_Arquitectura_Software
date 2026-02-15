2827269bba8317c9f2821bc9c6e3840fc5d94d3f
Hever <haalfonsoj@eafit.edu.co>
2026-02-15 13:52:41 -0500

    Add price attribute to products and display price in product detail view (Activity 3)

--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -30,10 +30,10 @@ class AboutPageView(TemplateView):
     
 class Product:
     products = [
-        {"id":"1", "name":"TV", "description":"Best TV"},
-        {"id":"2", "name":"iPhone", "description":"Best iPhone"},
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
-        {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+        {"id":"1", "name":"TV", "description":"Best TV", "price": 1200},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price": 999},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price": 49},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price": 89},
     ]
 
 class ProductIndexView(View):
